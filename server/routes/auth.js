93fb172f036a7d42fe50a207939f52ee810df3c7
Aryan Mehta <aryannmehta@gmail.com>
2023-03-22 21:03:12 +0530

    Remove alerts

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,29 +9,24 @@ router.post("/", async (req, res) => {
   try {
     const { error } = validate(req.body);
     if (error) {
-      alert(error.details[0].message);
       return res.status(400).send({ message: error.details[0].message });
     }
 
     const user = await UserInfo.findOne({ username: req.body.username});
     if (!user) {
-      alert("Invalid Email or Password");
       return res.status(401).send({ message: "Invalid Email or Password" });
     }
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) {
-      alert("Invalid Email or Password");
       return res.status(401).send({ message: "Invalid Email or Password" });
     }
 
     const token = user.generateAuthToken();
     res.status(200).send({ status: "Ok", data: [token, user.isAdmin], message: "Logged in successfully!" });
-    alert("Logged in Successfully");
 
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
-    alert("Internal Server Error");
   }
 });
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -10,13 +10,11 @@ router.post("/", async (req, res) => {
   try {
     const { error } = validate(req.body);
     if (error) {
-      alert(error.details[0].message);
       return res.status(400).send({ message: error.details[0].message });
     }
 
     const user = await UserInfo.findOne({ username: req.body.username });
     if (user) {
-      alert("User with given username already exists!");
       return res.status(409).send({ message: "User with given username already exists!" });
     }
 
@@ -25,10 +23,8 @@ router.post("/", async (req, res) => {
 
     await new UserInfo({ ...req.body, password: hashPassword }).save();
     res.status(200).send({ message: "User created successfully!" });
-    alert("User Created Successfully!");
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
-    alert("Internal Server Error");
   }
 });
 
